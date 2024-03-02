fea042ba01fc8ea62c38994589e7d4b927369afe
Priyadharshini Murugan <priyadharshini.murugan@relevantz.com>
2024-03-02 11:25:53 +0530

    Add project files.

---
+++ LoginSignup/Models/UserSignUpModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LoginSignup.Models
+{
+    public class UserSignUpModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "FirstName is required")]
+        public string? FirstName { get; set; }
+
+        [Required(ErrorMessage = "LastName is required")]
+        public string? LastName { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email address")]
+        public string? Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [MinLength(6, ErrorMessage = "Password must be at least 6 characters long")]
+        public string? Password { get; set; }
+
+        [Required(ErrorMessage = "Confirm Password is required")]
+        [Compare("Password", ErrorMessage = "Passwords do not match")]
+        public string? ConfirmPassword { get; set; }
+
+    }
+}
